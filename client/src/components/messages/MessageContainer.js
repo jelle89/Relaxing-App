32027d0e356496e3d88f3bcdcdb2ca16342c4fb1
jelle89 <jelle.monen@hotmail.com>
2019-07-30 22:36:40 +0200

    Messages working for relax

--- client/src/components/messages/MessageContainer.js
+++ client/src/components/messages/MessageContainer.js
@@ -1,20 +1,55 @@
 import React from "react";
+import {
+  FormControl,
+  InputLabel,
+  Input,
+  FormHelperText
+} from "@material-ui/core";
 
 export default function MessageContainer(props) {
-  console.log(props, 'propsvanmessages')
+  console.log(props, "propsvanmessages");
   return (
-    <form onSubmit={props.onSubmit}>
-    
-      <label>
-        Message:
-        <input
-          type="text"
-          name="text"
-          onChange={props.onChange}
-          value={props.text}
-        />
-      </label>
-      <button onClick={props.onSubmit}>Submit</button>
-    </form>
+    <div>
+    <FormControl>
+      <InputLabel htmlFor="text">Enter your message here</InputLabel>
+      <Input
+        type="text"
+        aria-describedby="my-helper-text"
+        name="text"
+        onChange={props.onChange}
+        value={props.text}
+      />
+      <button onClick={props.onSubmit}>Make it a nice one!</button>
+
+
+    </FormControl>
+
+      </div>
   );
 }
+
+{
+  /* <FormControl>
+          <InputLabel htmlFor="my-input">Email address</InputLabel>
+          <Input id="my-input" aria-describedby="my-helper-text" />
+          <FormHelperText id="my-helper-text">
+            We'll never share your email.
+          </FormHelperText>
+        </FormControl> */
+}
+
+//   <form onSubmit={props.onSubmit}>
+
+//   <label>
+//     Message:
+//     <input
+//       type="text"
+//       name="text"
+//       onChange={props.onChange}
+//       value={props.text}
+//     />
+//   </label>
+//   <button onClick={props.onSubmit}>Submit</button>
+// </form>
+
+// <button onClick={props.onSubmit}>Submit</button>
